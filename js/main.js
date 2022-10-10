c6942363f6d374fdc2868274eb79b5ea23482b4f
AlexRicartR <alex.ricart@outlook.com>
2022-10-10 22:20:57 +0200

    Including some DOM magic

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 function place(box) {
     alert ("Hey buddy, you clicked here!");
 }
+
+/*Setting that the inner text of every <div></div> will be that one stored under the "currentPlayer" variable */
+
+let currentPlayer = "O";
+function place(box) {
+box.innerText = currentPlayer;
+/*Here it is placed an if statement to the above function that will check if a player should place a nought or a cross.*/
+if(currentPlayer == "O") {
+    currentPlayer = "X";
+}
+else {
+    currentPlayer = "O";
+}
+}
