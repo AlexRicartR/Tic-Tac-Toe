4499adc5fa52ec4fc27242544afcf7d509a764ff
AlexRicartR <alex.ricart@outlook.com>
2022-10-11 06:54:52 +0200

    Adding a ternary operator to simplify if and else statements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 
 
-function place(box) {
-    alert ("Hey buddy, you clicked here!");
-}
+// function place(box) {
+//     alert ("Hey buddy, you clicked here!");
+// }
 
 /*Setting that the inner text of every <div></div> will be that one stored under the "currentPlayer" variable */
 
 let currentPlayer = "O";
 function place(box) {
-box.innerText = currentPlayer;
-/*Here it is placed an if statement to the above function that will check if a player should place a nought or a cross.*/
-if(currentPlayer == "O") {
-    currentPlayer = "X";
+    if (box.innerText != "") return;
+    box.innerText = currentPlayer;
+    currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
 }
-else {
-    currentPlayer = "O";
-}
-}
+    /*Here it is placed an if statement to the above function that will check if a player should place a nought or a cross.*/
+    
+
+/* Basically, through the above lines we are telling the program that if the condition of the currentPlayer variable is equal to O, then set the currentPlayer variable to X. Otherwise, if the condition is false, set it to O. */ 
