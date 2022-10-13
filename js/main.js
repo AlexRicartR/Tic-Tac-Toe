c5b110eda875500e5ccd3b20f4234f4b91cb379c
alexricart92 <alex.ricart@outlook.com>
2022-10-13 20:08:09 +0200

    Updating structure

--- js/main.js
+++ js/main.js
@@ -4,36 +4,68 @@ let player2Mark = "o"
 let player2MarkImg = "../img/imgo.png"
 let playerMark = player1Mark
 let board = document.getElementById('gameBoard')
-let player1 = {nombre: 'jugadorA', tipo:'humano'}
-let player2 = {nombre: 'jugadorB', tipo:'humano'}
+let player1 = { nombre: 'jugadorA', tipo: 'humano' }
+let player2 = { nombre: 'jugadorB', tipo: 'humano' }
 let player = {}
 
+// document.getElementsByClassName("startButton").disabled = false;
+
 const startGame = () => {
-        for (let i = 0; i < 9; i++){
+
+    let excludedSquares = [];
+
+    for (let i = 0; i < 9; i++) {   // i != excludedSquares
         let box = document.createElement("div")
-        box.id = i
-        box.className = 'board-box'
+        box.id = i;
+        box.className = 'board-box';
+        console.log("h")
 
         box.onclick = () => {
-            if (box.innerHTML == "") {
-                if (playerMark == player1Mark) {
-                    console.log(`url("${player1MarkImg}")`)
-                    box.style.backgroundImage = `url('${player1MarkImg}')`;
-                    player = player1;
-                    // player1Turns--;
-                    // player1TurnsCounter();
-                    // checkWinner();
-                    playerMark = player2Mark;
-                } else {
-                    box.style.backgroundImage = `url("${player2MarkImg}")`;
-                    player = player2;
-                    // player2Turns--;
-                    // player2TurnsCounter();
-                    // checkWinner();
-                    playerMark = player1Mark;
-                }
+
+            box.style.backgroundImage = `url('${player2MarkImg}')`;
+            excludedSquares.push(box.id);
+
+
+            const createNumberRandom = () => {
+                return Math.floor(Math.random() * (9 - 1))
             }
+
+
+            const numberRandom = () => {
+
+                let exist = false
+                let number = createNumberRandom();
+
+                excludedSquares.forEach(element => {
+                    if (element == number) {
+                    }
+                    else {
+//
+                    }
+                }
+                );
+                return number;
+            }
+
+            const Computer = () => {
+                const myElement = document.getElementById(numberRandom())
+
+                //  if(box.id === excludedSquares){
+                //}
+                // Math.floor(Math.random() * (9 - 1))
+                // excludedSquares.forEach(element => console.log(element))
+
+
+                excludedSquares.push(myElement.id);
+                myElement.style.backgroundImage = `url('${player1MarkImg}')`;
+            }
+
+            setTimeout(Computer, 700);
+
         }
+
+
+
 
         board.appendChild(box)
 
